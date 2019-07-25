ee00f7a247efa5bf7e9dbea4218b1ad80991f134
Cristian <CristianGualteros23@gmail.com>
2019-07-25 06:51:05 -0500

    Primer commit

---
+++ Login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>CLÍNICA DENTÁL TÚ MEJOR SONRISA</title>
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
+
+<div class="login">
+  <img  class="avatar"src="img/Captura1.png" alt="LOGIN">
+  <h1>INICIAR SESIÓN </h1><br>
+  <form >
+    <label for="Usuario">Usuario</label><br>
+<input type="text" name="txtNombre" placeholder="Usuario"><br><br>
+<label for="Contraseña">Contraseña</label><br>
+<input type="password" name="txtPass" placeholder="Contraseña"><br><br>
+<input type="submit" name="" value="INGRESAR"><br><br>
+<a href="">¿Haz olvidado tú Contraseña ?</a><br><br>
+<a href="">¿No tienes una cuenta?</a>
+
+  </form>
+</div>
+  </body>
+</html>
